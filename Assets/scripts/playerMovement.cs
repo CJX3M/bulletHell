911ce071809ddce5d466a8a199b9ce0144087b74
Carlos López <cjlc@hotmail.com>
2018-09-20 15:42:07 -0700

    Initialization
    Initialization
    Added a background and a player with basic movement

---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed = 0.0f;
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        var rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = movement * speed;
+    }
+   
+}
