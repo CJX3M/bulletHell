e6ab76568aea3670eaa0913d0e352fe16487f251
Carlos <cjlc@hotmail.com>
2019-08-12 14:07:34 -0700

    Finite State added
    Added a Finite state as well as a restart game option

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
@@ -10,24 +11,43 @@ public class GameController : MonoBehaviour
     public float startWait;
     public float waveWait;
     public float spawnWait;
+    public int score = 0;
 
     private float nextSpawn = 0.5f;
-    public int score = 0;
     private float myTime;
+    private bool gameOver;
+    private bool restart;
 
     public Text ScoreText;
+    public Text GameOverText;
+    public Text RestartGameText;
 
     private void Start()
     {
+        gameOver = false;
+        restart = false;
+        GameOverText.text = "";
+        RestartGameText.text = "";
         StartCoroutine(SpawnEnemies());
         UpdateScore(0);
     }
 
+    private void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
     IEnumerator SpawnEnemies()
     {
-        while (true)
+        yield return new WaitForSeconds(startWait);
+        while (!gameOver)
         {
-            yield return new WaitForSeconds(startWait);
             for (int i = 0; i < GetComponent<ObjectPool>().poolAmmount; i++)
             {
                 GameObject clone = GetComponent<ObjectPool>().GetPooledObject();
@@ -38,6 +58,8 @@ public class GameController : MonoBehaviour
                     clone.GetComponent<EnemyAim>().SetPlayerShip(playerShip);
                     clone.GetComponent<EnemyShot>().fire = true;
                     clone.SetActive(true);
+                    if (!playerShip.GetComponent<Health>().IsAlive())
+                        GameOver();
                     yield return new WaitForSeconds(spawnWait);
                 }
             }
@@ -49,5 +71,13 @@ public class GameController : MonoBehaviour
     {
         score += addScore;
         ScoreText.text = "Score: " + score.ToString().PadLeft(7, '0');
+    }
+
+    private void GameOver ()
+    {
+        gameOver = true;
+        restart = true;
+        GameOverText.text = "Game Over";
+        RestartGameText.text = "Press ENTER to start a new Game";
     }
 }
